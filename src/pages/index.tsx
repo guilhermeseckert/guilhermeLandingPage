345c3a84717c58418385494ad82e181d6429902d
Guilherme Eckert <guilherme-sonnemann@hotmail.com>
2021-09-21 02:49:21 -0700

    add section about

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,14 +2,26 @@ import { Header } from "../components/Header";
 import Image from "next/image";
 import avatar from "../assets/avatar.svg";
 import {
+  DiReact,
+  DiNodejs,
+  DiJavascript,
+  DiPostgresql,
+  DiCss3,
+  DiSass,
+} from "react-icons/di";
+
+import {SiTypescript} from 'react-icons/si'
+
+import {
   Flex,
   Text,
-  Button,
+  Icon,
   Box,
+  HStack,
   Circle,
   Grid,
   VStack,
-  GridItem,
+  Container,
   Spacer,
   color,
 } from "@chakra-ui/react";
@@ -30,29 +42,41 @@ export default function Home() {
         justifyContent="center"
       >
         <Box width={600}>
-          <Text fontSize="4xl" fontWeight="bold" p={2}>
-            Hello
-          </Text>
-          <Text fontSize="3xl" fontWeight="bold" p={2}>
-            I AM GUILHERME ECKERT
-          </Text>
-          <Text p={2}>
+          <Container>
+            <Text fontSize="4xl" fontWeight="bold">
+              Hello,
+            </Text>
+            <Text fontSize="3xl" fontWeight="bold">
+              I am Guilherme Eckert
+            </Text>
             lorem ipsum dolor sit amet, consectetur Lorem ipsum dolor sit amet
             consectetur adipisicing elit. Architecto perferendis aliquam
             asperiores aspernatur reprehenderit, doloremque quis aliquid velit
             atque ratione quas? Vitae minima, velit laboriosam impedit sequi
             architecto repudiandae exercitationem?
-          </Text>
-
-          <Box as="button" bg="#8257E5" p={6} w={250} _hover={{bg: "#6846b7"}} border={40} fontWeight="bold" fontSize="xl"> Contact me</Box>
+          </Container>
+          <Box
+            as="button"
+            bg="#8257E5"
+            p={6}
+            ml={40}
+            mt={30}
+            w={250}
+            _hover={{ bg: "#6846b7", transition: "1s" }}
+            style={{ borderRadius: "10px" }}
+            fontWeight="bold"
+            fontSize="xl"
+          >
+            {"Contact me"}
+          </Box>
         </Box>
 
         <Box width={632} height={656} align="center">
-          <Circle size={600} color="white">
+          <Circle size={600}>
             <VStack spacing={50} position="absolute" ml={-520}>
               <Circle
                 size={70}
-                bg="black"
+                bg="#3B3B3B"
                 position="relative"
                 color="#44D361"
                 fontSize="2xl"
@@ -62,7 +86,7 @@ export default function Home() {
               </Circle>
               <Circle
                 size={70}
-                bg="black"
+                bg="#3B3B3B"
                 position="relative"
                 color="#44D361"
                 fontSize="2xl"
@@ -81,7 +105,7 @@ export default function Home() {
             </Circle>
             <Circle
               size={70}
-              bg="black"
+              bg="#3B3B3B"
               position="absolute"
               ml={530}
               color="#44D361"
@@ -93,7 +117,7 @@ export default function Home() {
 
             <Circle
               size={70}
-              bg="black"
+              bg="#3B3B3B"
               position="absolute"
               mt={-500}
               ml={150}
@@ -105,7 +129,7 @@ export default function Home() {
             </Circle>
             <Circle
               size={70}
-              bg="black"
+              bg="#3B3B3B"
               position="absolute"
               mt={-350}
               ml={-410}
@@ -122,31 +146,158 @@ export default function Home() {
       <Flex
         as="section"
         width="99vw"
-        height="100vh"
+        p={20}
         bg="#363636"
-        mx="auto"
-        mt={5}
         display="flex"
+        flexDirection="column"
         align="center"
         justifyContent="center"
       >
-        <Box>
-          <Grid
-            h={500}
-            w={500}
-            templateRows="repeat(2, 1fr)"
-            templateColumns="repeat(6, 1fr)"
-            gap={6}
+        <HStack>
+          <Box
+            w={230}
+            h={230}
+            boxShadow="dark-lg"
+            bg="#2A2A2A"
+            fontWeight="bold"
+            fontSize="3xl"
+            p={4}
+            style={{ borderRadius: "10px" }}
+            centerAlign="center"
+            textAlign="center"
+            justifyContent="center"
           >
-            <GridItem colSpan={2} bg="papayawhip">
+            Front-End
+            <Icon as={DiReact} boxSize={40} color="#00D8FF" />
+          </Box>
+
+          <Box
+            w={230}
+            h={230}
+            bg="#2A2A2A"
+            boxShadow="dark-lg"
+            fontWeight="bold"
+            fontSize="3xl"
+            p={4}
+            style={{ borderRadius: "10px" }}
+            centerAlign="center"
+            textAlign="center"
+            justifyContent="center"
+          >
+            Back-End
+            <Icon as={DiNodejs} boxSize={40} color="#44D361" />
+          </Box>
+
+          <Box
+            w={230}
+            h={230}
+            bg="#2A2A2A"
+            boxShadow="dark-lg"
+            fontWeight="bold"
+            fontSize="3xl"
+            p={4}
+            style={{ borderRadius: "10px" }}
+            centerAlign="center"
+            textAlign="center"
+            justifyContent="center"
+          >
+            Postgres
+            <Icon as={DiPostgresql} boxSize={40} color="gray.50"></Icon>
+          </Box>
+          <Box
+            w={230}
+            h={230}
+            bg="#2A2A2A"
+            fontWeight="bold"
+            boxShadow="dark-lg"
+            fontSize="3xl"
+            p={4}
+            style={{ borderRadius: "10px" }}
+            textAlign="center"
+            centerAlign="center"
+            justifyContent="center"
+          >
+            JavaScript
+            <Icon as={DiJavascript} boxSize={40} color="yellow"></Icon>
+          </Box>
+        </HStack>
+
+        <HStack m={10}>
+          <Box
+            w={230}
+            h={230}
+            bg="#2A2A2A"
+            fontWeight="bold"
+            fontSize="3xl"
+            boxShadow="dark-lg"
+            p={4}
+            style={{ borderRadius: "10px" }}
+            textAlign="center"
+            centerAlign="center"
+            justifyContent="center"
+          >
+            Sass
+            <Icon as={DiSass} boxSize={40} color="#CE679B" />
+          </Box>
+
+          <Box
+            w={230}
+            h={230}
+            bg="#2A2A2A"
+            fontWeight="bold"
+            boxShadow="dark-lg"
+            fontSize="3xl"
+            p={4}
+            style={{ borderRadius: "10px" }}
+            textAlign="center"
+            centerAlign="center"
+            justifyContent="center"
+          >
+            CSS
+            <Icon as={DiCss3} boxSize={40} color="#264BDD"/>
+          </Box>
+          <Box
+            w={230}
+            h={230}
+            bg="#2A2A2A"
+            fontWeight="bold"
+            fontSize="3xl"
+            boxShadow="dark-lg"
+            p={4}
+            style={{ borderRadius: "10px" }}
+            textAlign="center"
+            centerAlign="center"
+            justifyContent="center"
+          >
+            TypeScript
+            <Icon as={SiTypescript} boxSize={40} color="#264BDD"/>
+          </Box>
+        </HStack>
+
+        <Box
+          display="flex"
+          justify="center"
+          align="center"
+          flex-direction="column"
+          mt={70}
+  
+        >
+          <Text bg="black" position="absolute">
+            {" "}
+            haususauhsauhhsuauhas{" "}
+          </Text>
+          <Circle size={600} bg="white">
+            {" "}
+          </Circle>
+          <VStack>
+            <Circle size={230} bg="white">
               {" "}
-            </GridItem>
-            <GridItem colSpan={2} bg="papayawhip" />
-            <GridItem colSpan={2} bg="papayawhip" />
-            <GridItem colSpan={2} bg="tomato" />
-            <GridItem colSpan={2} bg="tomato" />
-            <GridItem colSpan={2} bg="tomato" />
-          </Grid>
+            </Circle>
+            <Spacer />
+            <Circle size={230} bg="white">
+              {" "}
+            </Circle>
+          </VStack>
         </Box>
       </Flex>
     </>
